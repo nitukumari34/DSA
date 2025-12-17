f5c68c61ad5538e73649a598a7c1006f48aeb4a4
Nitu <nitu.kumari@sisindia.com>
2025-12-17 15:33:25 +0530

    Reverse LinkedList

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,60 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+class ListNode {
+    int value;
+    ListNode next;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    ListNode(int value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+public class Main {
+    public  static  ListNode reverseList(ListNode head){
+      ListNode temp=head;
+      ListNode prev=null;
+
+      while(temp!=null){
+          ListNode curr=temp.next;
+          temp.next=prev;
+          prev=temp;
+          temp=curr;
+
+      }
+      return  prev;
+    }
+
+    public  static  ListNode reverseBetween(ListNode head){
+        ListNode temp=head;
+        ListNode prev=null;
+
+        while(temp!=null){
+            ListNode curr=temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=curr;
+
         }
+        return  prev;
+    }
+
+    public  static  void printlinkedList(ListNode head){
+        while(head!=null){
+            System.out.print(head.value +"->");
+            head=head.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+     ListNode head=new ListNode(8);
+     ListNode first=new ListNode(7);
+     ListNode second=new ListNode(4);
+     ListNode fourth=new ListNode(6);
+     head.next=first;
+     first.next=second;
+     second.next=fourth;
+        printlinkedList(head);
+     head=reverseList(head);
+     printlinkedList(head);
     }
 }
