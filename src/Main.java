ffa6973d4566fe83268c8d2c3ebdf947e100be63
Nitu <nitu.kumari@sisindia.com>
2025-12-17 15:48:07 +0530

    reverseBetween and reverseKGroup

--- src/Main.java
+++ src/Main.java
@@ -22,6 +22,58 @@ public class Main {
       return  prev;
     }
 
+    public static ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummyNode=new ListNode(0);
+        dummyNode.next=head;
+        ListNode prev=dummyNode;
+        if(head==null || left==right) return  head;
+
+        for(int i=1;i<left;i++){
+            prev=prev.next;
+        }
+
+        ListNode curr=prev.next;
+        ListNode forw=curr.next;
+
+        for(int i=0;i<right-left;i++){
+            curr.next=forw.next;
+            forw.next=prev.next;
+            prev.next=forw;
+            forw=curr.next;
+
+        }
+
+        return  dummyNode;
+    }
+
+    public static ListNode reverseKGroup(ListNode head, int k) {
+        if(head==null){
+            return null;
+        }
+        ListNode  KSizeChecker=head;
+        for(int i=0;i<k;i++)
+        {
+            if(KSizeChecker==null)
+                return head;
+            KSizeChecker = KSizeChecker.next;
+        }
+
+        ListNode curr=head;
+        ListNode temp=head;
+        ListNode prev=null;
+        int count=1;
+        while(count<=k ){
+            temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
+            count++;
+        }
+        if(temp!=null)
+            head.next=reverseKGroup(temp,k);
+
+        return prev;
+    }
     public  static  void printlinkedList(ListNode head){
         while(head!=null){
             System.out.print(head.value +"->");
@@ -40,7 +92,12 @@ public class Main {
      first.next=second;
      second.next=fourth;
         printlinkedList(head);
-     head=reverseList(head);
+//     head=reverseList(head);
+//     printlinkedList(head);
+
+//     head=reverseBetween(head,2,3);
+//     printlinkedList(head);
+     head=reverseKGroup(head,2);
      printlinkedList(head);
     }
 }
